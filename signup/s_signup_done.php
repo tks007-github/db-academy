97c2166ca5952b2f3e5d31ae2f88667ebefc4dbc
tks007-github <takeshi.n.2020@gmail.com>
2021-07-20 11:42:26 +0900

    update

--- signup/s_signup_done.php
+++ signup/s_signup_done.php
@@ -27,26 +27,37 @@
 				break;
 		}
 
-		# player_managementデータベースに接続
+		// player_managementデータベースに接続
 		$dsn = 'mysql:dbname=player_management;host=localhost;charset=utf8mb4';
 		$user = 'root';
 		$password = 'root';
 		$dbh = new PDO($dsn, $user, $password);
 		$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-		# mst_playerテーブルに対してINSERT文で選手の追加
-		$sql = 'INSERT INTO mst_player (belong, name, password) VALUES (?,?,?)';
-		$stmt = $dbh->prepare($sql);
-		$data[0] = $belong;
-		$data[1] = $name;
-		$data[2] = $pass;
-		$stmt->execute($data);
+		// mst_playerテーブルに対してINSERT文で選手の追加
+		$sql1 = 'INSERT INTO mst_player (name, password, belong) VALUES (?,?,?)';
+		$stmt1 = $dbh -> prepare($sql1);
+		$data1[0] = $name;
+		$data1[1] = $pass;
+		$data1[2] = $belong;
+		$stmt1 -> execute($data1);
 
-		# player_managementデータベースから切断
+		// mst_playerテーブルから選手のIDを取得
+		$sql2 = 'SELECT id FROM mst_player WHERE name = ? and password = ? and belong = ?';
+		$stmt2 = $dbh -> prepare($sql2);
+		$data2[0] = $name;
+		$data2[1] = $pass;
+		$data2[2] = $belong;
+		$stmt2 -> execute($data2);
+		$rec2 = $stmt2 -> fetch(PDO::FETCH_ASSOC);
+
+		// player_managementデータベースから切断
 		$dbh = null;
 
-		print $user_name . 'さん登録ありがとうございます。<br />';
-		print '<br />';
+		print '以下の情報を登録しました。<br>';
+		print '下記のリンクからログインし、問診票の入力を完了してください。<br>';
+		print '<br>';
+		print '会員コード：' . $code . $rec2['id'] . '<br>'
 	} catch (Exception $e) {
 		var_dump($e);
 		exit();
