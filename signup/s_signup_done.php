f2861a92c7485405259954484a65000283a81184
tks007-github <takeshi.n.2020@gmail.com>
2021-07-20 11:57:08 +0900

    update

--- signup/s_signup_done.php
+++ signup/s_signup_done.php
@@ -36,20 +36,20 @@
 
 		// mst_playerテーブルに対してINSERT文で選手の追加
 		$sql1 = 'INSERT INTO mst_player (name, password, belong) VALUES (?,?,?)';
-		$stmt1 = $dbh -> prepare($sql1);
+		$stmt1 = $dbh->prepare($sql1);
 		$data1[0] = $name;
 		$data1[1] = $pass;
 		$data1[2] = $belong;
-		$stmt1 -> execute($data1);
+		$stmt1->execute($data1);
 
 		// mst_playerテーブルから選手のIDを取得
 		$sql2 = 'SELECT id FROM mst_player WHERE name = ? and password = ? and belong = ?';
-		$stmt2 = $dbh -> prepare($sql2);
+		$stmt2 = $dbh->prepare($sql2);
 		$data2[0] = $name;
 		$data2[1] = $pass;
 		$data2[2] = $belong;
-		$stmt2 -> execute($data2);
-		$rec2 = $stmt2 -> fetch(PDO::FETCH_ASSOC);
+		$stmt2->execute($data2);
+		$rec2 = $stmt2->fetch(PDO::FETCH_ASSOC);
 
 		// player_managementデータベースから切断
 		$dbh = null;
@@ -58,14 +58,17 @@
 		print '下記のリンクからログインし、問診票の入力を完了してください。<br>';
 		print '<br>';
 		print '会員コード：' . $ini . $rec2['id'] . '<br>';
-	} 
-	catch (Exception $e) 
-	{
+		print '氏名：' . $name . '<br>';
+		print '所属：' . $belong . '<br>';
+	} catch (Exception $e) {
 		var_dump($e);
 		exit();
 	}
 
 	?>
+
+	<br>
+	<a href="../p_login.html"> 選手管理</a>
 
 </body>
 
