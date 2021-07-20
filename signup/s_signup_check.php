5c8b844a9b8fc401733aba243b9a2ceb75d95727
tks007-github <takeshi.n.2020@gmail.com>
2021-07-20 10:37:09 +0900

    update

--- signup/s_signup_check.php
+++ signup/s_signup_check.php
@@ -11,15 +11,14 @@
 <body>
 
 	<?php
-	
+
 	print '下記の内容で登録を行います。<br>';
 	print '<br>';
 
-	// s_signup2.phpから渡された値をサニタイズ
+	// s_signup.phpから渡された値をサニタイズ
+	$belong = htmlspecialchars($_POST['belong'], ENT_QUOTES, 'UTF-8');
 	$name = htmlspecialchars($_POST['name'], ENT_QUOTES, 'UTF-8');
 	$pass = htmlspecialchars($_POST['pass'], ENT_QUOTES, 'UTF-8');
-	$height = htmlspecialchars($_POST['height'], ENT_QUOTES, 'UTF-8');
-	$weight = htmlspecialchars($_POST['weight'], ENT_QUOTES, 'UTF-8');
 
 	print '所属：' . $belong . '<br>';
 	print '氏名：' . $name . '<br>';
@@ -30,8 +29,7 @@
 	print '<input type="hidden" name="name" value="' . $name . '">';
 	print '<input type="hidden" name="pass" value="' . $pass . '">';
 	print '<input type="hidden" name="height" value="' . $height . '">';
-	print '<input type="hidden" name="weight" value="' . $weight . '">';
-	print '<br />';
+	print '<br>';
 	print '<input type="button" onclick="history.back()" value="戻る">';
 	print '<input type="submit" value="登録">';
 	print '</form>';
