78472b4da57df0094ab3ae4ec1caec8f89ff64c7
tks007-github <takeshi.n.2020@gmail.com>
2021-07-21 11:06:21 +0900

    update

--- player_management/player/p_login_check.php
+++ player_management/player/p_login_check.php
@@ -1,8 +1,9 @@
 <?php
 try
 {
-	// s_login.htmlからパスワードを受け取る
-	$mst_pass = htmlspecialchars($_POST['mst_pass'], ENT_QUOTES, 'UTF-8');
+	// p_login.htmlからパスワードを受け取る
+	$p_code = htmlspecialchars($_POST['p_code'], ENT_QUOTES, 'UTF-8');
+	$p_pass = htmlspecialchars($_POST['p_pass'], ENT_QUOTES, 'UTF-8');
 
 	// player_managementデータベースに接続する
 	$dsn = 'mysql:dbname=player_management;host=localhost;charset=utf8';
@@ -11,24 +12,25 @@ try
 	$dbh = new PDO($dsn, $user, $password);
 	$dbh -> setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
-	// mst_passwordテーブルからパスワードを使ってIDを検索
-	$sql = 'SELECT id FROM mst_password WHERE password = ?';
+	// playerテーブルから会員コードとパスワードを使って名前を検索
+	$sql = 'SELECT player_name FROM player WHERE player_code = ? AND player_password = ?';
 	$stmt = $dbh -> prepare($sql);
-	$data[0] = $mst_pass;
+	$data[0] = $p_code;
+	$data[1] = $p_pass;
 	$stmt -> execute($data);
 	$rec = $stmt -> fetch(PDO::FETCH_ASSOC);
-	
+
 	// player_managementデータベースから切断する
 	$dbh = null;
 
-	if ($rec['id'] == 1) {				// データベースからの問い合わせ結果があった場合
+	if (isset($rec)) { 				    // データベースからの問い合わせ結果があった場合
 		session_start();						// セッションを開始
 		$_SESSION['login'] = 1;					// セッション変数に値を格納
-		header('Location:s_signup.html');		// s_signup.htmlへリダイレクト
+		header('Location:p_top.html');			// p_top.htmlへリダイレクト
 		exit();
 	} else {							// データベースからの問い合わせ結果がない場合
 		print 'パスワードに間違いがあります。<br>';
-		print '<a href="s_login.html"> 戻る</a>';
+		print '<a href="p_login.html"> 戻る</a>';
 	}
 }
 
