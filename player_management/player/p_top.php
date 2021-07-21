41a0bed10f0d8320cfaaa37141b3fbb326ce7707
tks007-github <takeshi.n.2020@gmail.com>
2021-07-21 12:46:57 +0900

    update

--- player_management/player/p_top.php
+++ player_management/player/p_top.php
@@ -1,9 +1,14 @@
 <?php
     session_start();
+    session_regenerate_id(true);
     if (!isset($_SESSION['login'])) {
         print 'ログインされていません。<br>';
         print '<a href="s_login.html">ログイン画面へ</a>';
         exit();
+    } else {
+        print $_SESSION['p_code'];
+        print 'さんログイン中<br>';
+        print '<br>';
     }
 ?>
 
@@ -13,11 +18,9 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Player</title>
 </head>
 <body>
-
-    <h3>ログイン成功</h3>
     
 </body>
 </html>
--- player_management/signup/s_signup.php
+++ player_management/signup/s_signup.php
@@ -1,5 +1,6 @@
 <?php
     session_start();
+    session_regenerate_id(true);
     if (!isset($_SESSION['login'])) {
         print 'ログインされていません。<br>';
         print '<a href="s_login.html">ログイン画面へ</a>';
