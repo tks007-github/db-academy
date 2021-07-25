0b34c808b62f5460165de9169c2930ed1ecb7dc9
tks007-github <takeshi.n.2020@gmail.com>
2021-07-25 09:14:34 +0900

    update

--- player_management/player/p_phisical_info_edit_done.php
+++ player_management/player/p_phisical_info_edit_done.php
@@ -25,13 +25,16 @@ if (!isset($_SESSION['login'])) {
 
 <body>
 
-    <h3>削除完了</h3>
+    <h3>編集完了</h3>
 
     <?php
     try {
-        // p_phisical_info_delete.phpから渡された値をサニタイズ
+        // p_phisical_info_edit.phpから渡された値をサニタイズ
         $id = htmlspecialchars($_POST['id'], ENT_QUOTES, 'UTF-8');
-        
+        $date = htmlspecialchars($_POST['date'], ENT_QUOTES, 'UTF-8');
+        $height = htmlspecialchars($_POST['height'], ENT_QUOTES, 'UTF-8');
+        $weight = htmlspecialchars($_POST['weight'], ENT_QUOTES, 'UTF-8');
+
         // player_managementデータベースに接続
         $dsn = 'mysql:dbname=player_management;host=localhost;charset=utf8mb4';
         $user = 'root';
@@ -39,10 +42,13 @@ if (!isset($_SESSION['login'])) {
         $dbh = new PDO($dsn, $user, $password);
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // phisical_infoテーブルのデータを削除
-        $sql = 'DELETE FROM phisical_info WHERE id = ?';
+        // phisical_infoテーブルのデータを更新
+        $sql = 'UPDATE phisical_info SET date = ?, height = ?, weight = ? WHERE id = ?';
         $stmt = $dbh->prepare($sql);
-        $data[0] = $id;
+        $data[0] = $date;
+        $data[1] = $height;
+        $data[2] = $weight;
+        $data[3] = $id;
         $stmt->execute($data);
 
         // player_managementデータベースから切断
