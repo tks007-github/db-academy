2edbda0f79ea5a5f1047730717e9491e32b41b97
tks007-github <takeshi.n.2020@gmail.com>
2021-07-27 12:18:40 +0900

    create

---
+++ player_management/manager/m_p_phisical_info_branch.php
@@ -0,0 +1,21 @@
+<?php
+
+if (isset($_POST['edit'])) {
+    if (!isset($_POST['id'])) {
+        header('Location: m_p_phisical_info_ng.php');
+        exit();
+    }
+    $id = $_POST['id'];
+    header('Location: m_p_phisical_info_edit.php?id=' . $id);
+    exit();
+}
+
+if (isset($_POST['delete'])) {
+    if (!isset($_POST['id'])) {
+        header('Location: m_p_phisical_info_ng.php');
+        exit();
+    }
+    $id = $_POST['id'];
+    header('Location: m_p_phisical_info_delete.php?id=' . $id);
+    exit();
+}
