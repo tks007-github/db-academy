931458d767f839e4db08942ad5751d031bb12b10
tks007-github <takeshi.n.2020@gmail.com>
2021-07-27 10:40:04 +0900

    update

---
+++ player_management/manager/m_top.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+    session_regenerate_id(true);
+    if (!isset($_SESSION['login'])) {
+        print 'ログインされていません。<br>';
+        print '<a href="m_login.html">ログイン画面へ</a>';
+        exit();
+    } else {
+        print $_SESSION['m_code'];
+        print 'さんログイン中<br>';
+        print '<br>';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manager</title>
+</head>
+<body>
+
+    <h3>トップページ</h3>
+    <br>
+    
+    <input type="button" onclick="location.href='m_logout.php'" value="ログアウト">
+
+</body>
+</html>
