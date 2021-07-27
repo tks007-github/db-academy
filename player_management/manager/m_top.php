cd70444da350b7d503ee4be33ffa0401b3bbffdc
tks007-github <takeshi.n.2020@gmail.com>
2021-07-27 11:11:56 +0900

    update

--- player_management/manager/m_top.php
+++ player_management/manager/m_top.php
@@ -26,6 +26,7 @@
     <br>
     
     <a href="m_search.php">選手検索</a><br>
+    <a href="m_phisical_test.php">新規フィジカルテスト作成</a><br>
     <br><br>
     <input type="button" onclick="location.href='m_logout.php'" value="ログアウト">
 
