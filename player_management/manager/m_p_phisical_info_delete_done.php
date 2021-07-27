6bed98a7f84ac11950e2bfebb30f561e04fb35f2
tks007-github <takeshi.n.2020@gmail.com>
2021-07-27 12:20:38 +0900

    create

---
+++ player_management/manager/m_p_phisical_info_delete_done.php
@@ -0,0 +1,64 @@
+<?php
+    session_start();
+    session_regenerate_id(true);
+    if (!isset($_SESSION['m_login'])) {
+        print 'ログインされていません。<br>';
+        print '<a href="m_login.html">ログイン画面へ</a>';
+        exit();
+    } else {
+        print $_SESSION['m_code'];
+        print 'さんログイン中<br>';
+        print '（検索条件：' . $_SESSION['p_code'] . '）';
+        print '<br>';
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Manager</title>
+</head>
+
+<body>
+
+    <h3>削除完了</h3>
+
+    <?php
+    try {
+        // m_p_phisical_info_delete.phpから渡された値をサニタイズ
+        $id = htmlspecialchars($_POST['id'], ENT_QUOTES, 'UTF-8');
+
+        // player_managementデータベースに接続
+        $dsn = 'mysql:dbname=player_management;host=localhost;charset=utf8mb4';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // phisical_infoテーブルのデータを削除
+        $sql = 'DELETE FROM phisical_info WHERE id = ?';
+        $stmt = $dbh->prepare($sql);
+        $data[0] = $id;
+        $stmt->execute($data);
+
+        // player_managementデータベースから切断
+        $dbh = null;
+
+    } catch (Exception $e) {
+        var_dump($e);
+        exit();
+    }
+
+    ?>
+
+    削除が完了しました<br>
+    <br>
+    <a href="m_p_phisical_info.php">身体情報トップ</a>
+
+</body>
+
+</html>
