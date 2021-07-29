e1accfb66ed00208039f16be921ac6f8382dc901
tks007-github <takeshi.n.2020@gmail.com>
2021-07-29 11:14:59 +0900

    update

--- player_management/player/p_phisical_test_add_done.php
+++ player_management/player/p_phisical_test_add_done.php
@@ -34,46 +34,80 @@ if (!isset($_SESSION['p_login'])) {
         $date = htmlspecialchars($_POST['date'], ENT_QUOTES, 'UTF-8');
         if (isset($_POST['test1'])) {
             $test1 = htmlspecialchars($_POST['test1'], ENT_QUOTES, 'UTF-8');
+            $test1_value = 1;
         } else {
             $test1 = '';
+            $test1_value = 0;
         }
         if (isset($_POST['test2'])) {
             $test2 = htmlspecialchars($_POST['test2'], ENT_QUOTES, 'UTF-8');
+            $test2_value = 1;
         } else {
             $test2 = '';
+            $test2_value = 0;
         }
         if (isset($_POST['test3'])) {
             $test3 = htmlspecialchars($_POST['test3'], ENT_QUOTES, 'UTF-8');
+            $test3_value = 1;
         } else {
             $test3 = '';
+            $test3_value = 0;
         }
 
-        var_dump($_POST);
-        // // player_managementデータベースに接続
-        // $dsn = 'mysql:dbname=player_management;host=localhost;charset=utf8mb4';
-        // $user = 'root';
-        // $password = 'root';
-        // $dbh = new PDO($dsn, $user, $password);
-        // $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        $test_result[] = $test1;
+        $test_result[] = $test2;
+        $test_result[] = $test3;
 
-        // // phisical_infoテーブルに新たなデータを登録
-        // $sql = 'INSERT INTO phisical_test (player_code, date, test1, test2, test3) VALUES (?, ?, ?, ?, ?)';
-        // $stmt = $dbh->prepare($sql);
-        // $data[0] = $p_code;
-        // $data[1] = $date;
-        // $data[2] = $test1;
-        // $data[3] = $test2;
-        // $data[4] = $test3;
-        // $stmt->execute($data);
+        $test_value[] = $test1_value;
+        $test_value[] = $test2_value;
+        $test_value[] = $test3_value;
 
-        // // player_managementデータベースから切断
-        // $dbh = null;
+        // player_managementデータベースに接続
+        $dsn = 'mysql:dbname=player_management;host=localhost;charset=utf8mb4';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-        // print '以下の情報を登録しました。<br>';
-        // print '<br>';
-        // print '日付：' . $date . '<br>';
-        // print '身長：' . $height . '<br>';
-        // print '体重：' . $weight . '<br>';
+        // phisical_infoテーブルに新たなデータを登録
+        $sql = 'INSERT INTO phisical_test (player_code, date, test1, test2, test3) VALUES (?, ?, ?, ?, ?)';
+        $stmt = $dbh->prepare($sql);
+        $data[0] = $p_code;
+        $data[1] = $date;
+        $data[2] = $test1;
+        $data[3] = $test2;
+        $data[4] = $test3;
+        $stmt->execute($data);
+
+        // phisical_test_itemテーブルから項目名を検索
+        $sql2 = '
+                SELECT test_value 
+                FROM phisical_test_item 
+                ORDER BY test_code
+                ';
+        $stmt2 = $dbh->prepare($sql2);
+        $stmt2->execute();
+
+        // player_managementデータベースから切断
+        $dbh = null;
+
+        print '以下の情報を登録しました。<br>';
+        print '<br>';
+        print '日付<br>';
+        print $date;
+        print '<br><br>';
+        print '項目<br>';
+
+        // test_codeのmax値(1～3)までforループをまわす
+        for ($i = 0; $i < 3; $i++) {
+            $rec2 = $stmt2->fetch(PDO::FETCH_ASSOC);
+            if ($test_value[$i]) {
+                print $rec2['test_value'] . '　';
+                print $test_result[$i];
+                print '<br>';
+            }
+            print '<br>';
+        }
     } catch (Exception $e) {
         var_dump($e);
         exit();
@@ -82,7 +116,7 @@ if (!isset($_SESSION['p_login'])) {
     ?>
 
     <br>
-    <a href="p_phisical_info.php">身体情報トップ</a>
+    <a href="p_phisical_test.php">フィジカルテストトップ</a>
 
 </body>
 
