9caec813180505503785ca7744f91397924eff42
tks007-github <takeshi.n.2020@gmail.com>
2021-08-12 18:30:00 +0900

    update

---
+++ development/player/p_phisical_info/p_phisical_info_delete_done.php
@@ -0,0 +1,63 @@
+<?php
+session_start();
+session_regenerate_id(true);
+if (!isset($_SESSION['p_login'])) {
+    print 'ログインされていません。<br>';
+    print '<a href="p_login.html">ログイン画面へ</a>';
+    exit();
+} else {
+    print $_SESSION['player_name'];
+    print 'さんログイン中<br>';
+    print '<br>';
+}
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>p_phisical_info_delete_done</title>
+</head>
+
+<body>
+
+    <h3>身体情報の削除完了</h3>
+
+    <?php
+    try {
+        // SESSIONでphisical_info_codeを受け取る
+        $phisical_info_code = $_SESSION['phisical_info_code'];
+        
+        // db_academyデータベースに接続する
+        $dsn = 'mysql:dbname=db_academy;host=localhost;charset=utf8';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // phisical_infoテーブルに情報を追加
+        $sql = '
+                DELETE FROM phisical_info  
+                WHERE phisical_info_code = ? 
+                ';
+        $stmt = $dbh->prepare($sql);
+        $data[] = $phisical_info_code;
+        $stmt->execute($data);
+
+        // player_managementデータベースから切断する
+        $dbh = null;
+
+        print '削除が完了しました<br><br>';
+        print '<input type="button" onclick="location.href=\'p_phisical_info_top.php\'" value="戻る">';
+    } catch (Exception $e) {
+        var_dump($e);
+        exit();
+    }
+    ?>
+
+</body>
+
+</html>
