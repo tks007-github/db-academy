ac6e8e5e4361e8137fd4c65712d3d79412ef81d2
tks007-github <takeshi.n.2020@gmail.com>
2021-08-20 16:04:07 +0900

    update

--- development/player/p_questionnaire/p_questionnaire_edit.php
+++ development/player/p_questionnaire/p_questionnaire_edit.php
@@ -3,7 +3,7 @@ session_start();
 session_regenerate_id(true);
 if (!isset($_SESSION['p_login'])) {
     print 'ログインされていません。<br>';
-    print '<a href="p_login.html">ログイン画面へ</a>';
+    print '<a href="../p_top/p_top_login.html">ログイン画面へ</a>';
     exit();
 } else {
     print $_SESSION['player_name'];
