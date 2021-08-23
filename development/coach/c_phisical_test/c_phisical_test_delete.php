e73f2b83e02add187dec8854e0035646a266260a
tks007-github <takeshi.n.2020@gmail.com>
2021-08-24 05:53:54 +0900

    update

---
+++ development/coach/c_phisical_test/c_phisical_test_delete.php
@@ -0,0 +1,140 @@
+<?php
+session_start();
+session_regenerate_id(true);
+if (!isset($_SESSION['c_login'])) {
+    print 'ログインされていません。<br>';
+    print '<a href="c_top_login.html">ログイン画面へ</a>';
+    exit();
+} else {
+    print $_SESSION['coach_name'];
+    print 'さんログイン中<br>';
+    print '<br>';
+}
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>c_phisical_test_delete</title>
+</head>
+
+<body>
+
+    <h3>フィジカルテストの削除</h3>
+    <br>
+
+    <?php
+
+    try {
+        // 自作の関数を呼び出す
+        require_once('../../function/function.php');
+        // POSTの中身をすべてサニタイズする
+        $post = sanitize($_POST);
+
+        $phisical_test_code = $post['phisical_test_code'];
+
+        // db_academyデータベースに接続する
+        $dsn = 'mysql:dbname=db_academy;host=localhost;charset=utf8';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        // phisical_testテーブルからphisical_test_codeを使って情報を検索
+        $sql = '
+                SELECT * 
+                FROM phisical_test 
+                WHERE phisical_test_code = ?
+                ';
+        $stmt = $dbh->prepare($sql);
+        $data[] = $phisical_test_code;
+        $stmt->execute($data);
+        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+        // データをそれぞれ変数で保持
+        $date = $rec['date'];
+        $belong_code = $rec['belong_code'];
+        $test1_boolean = $rec['10m走'];
+        $test2_boolean = $rec['20m走'];
+        $test3_boolean = $rec['30m走'];
+        $test4_boolean = $rec['50m走'];
+        $test5_boolean = $rec['1500m走'];
+        $test6_boolean = $rec['プロアジリティ'];
+        $test7_boolean = $rec['立ち幅跳び'];
+        $test8_boolean = $rec['メディシンボール投げ'];
+        $test9_boolean = $rec['垂直飛び'];
+        $test10_boolean = $rec['背筋力'];
+        $test11_boolean = $rec['握力'];
+        $test12_boolean = $rec['サイドステップ'];
+
+        // db_academyデータベースから切断する
+        $dbh = null;
+
+        print '本当に削除しますか<br><br>';
+
+        print '日付<br>';
+        print $date;
+        print '<br><br>';
+
+        print '所属<br>';
+        print $belong_code;
+        print '<br><br>';
+
+        print '項目<br>';
+        if ($test1_boolean) {
+            print '10m走<br>';
+        }
+        if ($test2_boolean) {
+            print '20m走<br>';
+        }
+        if ($test3_boolean) {
+            print '30m走<br>';
+        }
+        if ($test4_boolean) {
+            print '50m走<br>';
+        }
+        if ($test5_boolean) {
+            print '1500m走<br>';
+        }
+        if ($test6_boolean) {
+            print 'プロアジリティ<br>';
+        }
+        if ($test7_boolean) {
+            print '立ち幅跳び<br>';
+        }
+        if ($test8_boolean) {
+            print 'メディシンボール投げ<br>';
+        }
+        if ($test9_boolean) {
+            print '垂直飛び<br>';
+        }
+        if ($test10_boolean) {
+            print '背筋力<br>';
+        }
+        if ($test11_boolean) {
+            print '握力<br>';
+        }
+        if ($test12_boolean) {
+            print 'サイドステップ<br>';
+        }
+
+        
+        print '<br>';
+        print '<input type="button" onclick="location.href=\'c_phisical_test_list.php\'" value="戻る">';
+        print '<input type="button" onclick="location.href=\'c_phisical_test_delete_done.php\'" value="削除">';
+    } catch (Exception $e) {
+        var_dump($e);
+        exit();
+    }
+
+    ?>
+
+</body>
+
+</html>
--- development/coach/c_phisical_test/c_phisical_test_list.php
+++ development/coach/c_phisical_test/c_phisical_test_list.php
@@ -1,41 +1,90 @@
+<?php
+    session_start();
+    session_regenerate_id(true);
+    if (!isset($_SESSION['c_login'])) {
+        print 'ログインされていません。<br>';
+        print '<a href="c_top_login.html">ログイン画面へ</a>';
+        exit();
+    } else {
+        print $_SESSION['coach_name'];
+        print 'さんログイン中<br>';
+        print '<br>';
+    }
+?>
+
 <!DOCTYPE html>
 <html lang="ja">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>c_phisical_test_add</title>
+    <title>c_phisical_test_list</title>
 </head>
+
 <body>
 
-    C0001さんログイン中
-    <br><br>
-
     <h3>フィジカルテスト一覧</h3>
-    <br>
-    
-    <input type="radio">
-    日付：2021-07-07
-    <br>
-    <input type="radio">
-    日付：2021-06-02
-    <br>
 
-    <br><br>
-    
-    <br>
-    <a href="c_phisical_test_top.html">戻る(ボタン)</a><br>
-    <a href="c_phisical_test_content.html">ＯＫ(ボタン)</a>
+    <?php
+    // $_SESSIONを初期化
+    $_SESSION['phisical_test_code'] = '';
+    $_SESSION['belong_code'] = '';
+    $_SESSION['date'] = '';
+    $_SESSION['10m走_boolean'] = '';
+    $_SESSION['20m走_boolean'] = '';
+    $_SESSION['30m走_boolean'] = '';
+    $_SESSION['50m走_boolean'] = '';
+    $_SESSION['1500m走_boolean'] = '';
+    $_SESSION['プロアジリティ_boolean'] = '';
+    $_SESSION['立ち幅跳び_boolean'] = '';
+    $_SESSION['メディシンボール投げ_boolean'] = '';
+    $_SESSION['垂直飛び_boolean'] = '';
+    $_SESSION['背筋力_boolean'] = '';
+    $_SESSION['握力_boolean'] = '';
+    $_SESSION['サイドステップ_boolean'] = '';
 
-    <br><br><br>
-    <説明><br>
-    フィジカルテスト一覧画面です。
+    try {
+        // db_academyデータベースに接続する
+        $dsn = 'mysql:dbname=db_academy;host=localhost;charset=utf8';
+        $user = 'root';
+        $password = 'root';
+        $dbh = new PDO($dsn, $user, $password);
+        $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    <br><br>
+        // phisical_testテーブルから情報を検索
+        $sql = '
+                SELECT *
+                FROM phisical_test 
+                ORDER BY date DESC, belong_code
+                ';
+        $stmt = $dbh->prepare($sql);
+        $stmt->execute();
 
-    データベース(phisical_test_cordテーブル)に検索をかける。<br>
-    ラジオボタンにphisical_test_cord_codeを紐づけて、p_phisical_test_contentに送信。
+        // db_academyデータベースから切断する
+        $dbh = null;
 
-    
+        print '<form method="post" action="c_phisical_test_delete.php">';
+        while (true) {
+            $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+            if ($rec == '') {
+                break;
+            }
+            print '<input type="radio" name="phisical_test_code" value="' . $rec['phisical_test_code'] . '">';
+            print '日付：' . $rec['date'] . '　';
+            print '所属：' . $rec['belong_code'] . '　';
+            print '<br>';
+        }
+        print '<br><br>';
+        print '<input type="button" onclick="location.href=\'c_phisical_test_top.php\'" value="戻る">';
+        print '<input type="submit" value="削除">';
+
+    } catch (Exception $e) {
+        var_dump($e);
+        exit();
+    }
+    ?>
+
 </body>
+
 </html>
