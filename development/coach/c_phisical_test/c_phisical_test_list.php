29e158a1206395158789670df9c605c02fd14414
tks007-github <takeshi.n.2020@gmail.com>
2021-08-25 17:22:18 +0900

    update

--- development/coach/c_phisical_test/c_phisical_test_list.php
+++ development/coach/c_phisical_test/c_phisical_test_list.php
@@ -1,15 +1,15 @@
 <?php
-    session_start();
-    session_regenerate_id(true);
-    if (!isset($_SESSION['c_login'])) {
-        print 'ログインされていません。<br>';
-        print '<a href="../c_top/c_top_login.html">ログイン画面へ</a>';
-        exit();
-    } else {
-        print $_SESSION['coach_name'];
-        print 'さんログイン中<br>';
-        print '<br>';
-    }
+session_start();
+session_regenerate_id(true);
+if (!isset($_SESSION['c_login'])) {
+    print 'ログインされていません。<br>';
+    print '<a href="../c_top/c_top_login.html">ログイン画面へ</a>';
+    exit();
+} else {
+    print $_SESSION['coach_name'];
+    print 'さんログイン中<br>';
+    print '<br>';
+}
 ?>
 
 <!DOCTYPE html>
@@ -60,26 +60,89 @@
                 ';
         $stmt = $dbh->prepare($sql);
         $stmt->execute();
+        $rec = $stmt->fetchAll();
 
         // db_academyデータベースから切断する
         $dbh = null;
 
-        print '<form method="post" action="c_phisical_test_list_check.php">';
-        while (true) {
-            $rec = $stmt->fetch(PDO::FETCH_ASSOC);
-            if ($rec == '') {
-                break;
-            }
-            print '<input type="radio" name="phisical_test_code" value="' . $rec['phisical_test_code'] . '">';
-            print '日付：' . $rec['date'] . '　';
-            print '所属：' . $rec['belong_code'] . '　';
-            print '<br>';
-        }
-        print '<br><br>';
-        print '<input type="button" onclick="location.href=\'c_phisical_test_top.php\'" value="戻る">';
-        print '<input type="submit" name="delete" value="削除">';
-        print '<input type="submit" name="result" value="成績表">';
+        if (empty($rec)) {
+            print 'フィジカルテストの登録がありません';
+            print '<br><br>';
+            print '<input type="button" onclick="location.href=\'c_phisical_test_top.php\'" value="戻る">';
+        } else {
 
+            $record_max = 3;                        // 1ページあたりの最大レコード数
+            $record_num = count($rec);              // レコードの総数
+            $page_max = ceil($record_num / $record_max);    // 総ページ数の計算
+
+            if (!isset($_GET['page_id']))    // $_GET['page_id']が定義されてない場合
+            {
+                $page_now = 1;
+            } else                           // $_GET['page_id']が定義されている場合
+            {
+                $page_now = $_GET['page_id'];
+            }
+
+            $record_start = ($page_now - 1) * $record_max;            // ページに表示する最初のレコード番号（1個目を0番目とする）
+
+            print '<form method="post" action="c_phisical_test_list_check.php">';
+
+            // ページに表示する分のデータだけ切り取る
+            $disp_data = array_slice($rec, $record_start, $record_max, true);
+            foreach ($disp_data as $key => $value) {
+                print '<input type="radio" name="phisical_test_code" value="' . $value['phisical_test_code'] . '">';
+                print '日付：' . $value['date'] . '　';
+                print '所属：' . $value['belong_code'] . '　';
+                print '<br>';
+            }
+
+            print '全件数' . $record_num . '件（' . ($record_start + 1) . '件～';
+
+            if ($record_num >= $record_start + $record_max)        // 最後のレコードが現在のページに収まらない場合
+            {
+                print $record_start + $record_max;
+            } else                                                 // 最後のレコードが現在のページに収まる場合
+            {
+                print $record_num;
+            }
+
+            print '件表示中）　';
+
+            // if文によってリンクを貼るかどうかを決定
+            if ($page_now > 1)                        // 現在のページ番号が1より大きい場合
+            {
+                print '<a href=p_phisical_test_top.php?page_id=' . ($page_now - 1) . '>前へ</a>' . '　';
+            } else                                    // 現在のページ番号が1の場合（1未満になることはないため）
+            {
+                print '前へ' . '　';
+            }
+
+            // // ループを回してすべてのページ番号を表示する
+            // for ($i = 1; $i <= $page_max; $i++) {
+            //     // if文によってリンクを貼るかどうかを決定
+            //     if ($i == $page_now)                // $iと現在のページ番号が等しい場合			
+            //     {
+            //         print $page_now . '　';
+            //     } else                              // $iと現在のページ番号が等しくない場合
+            //     {
+            //         print '<a href=p_phisical_info_list.php?page_id=' . $i . '>' . $i . '</a>' . '　';
+            //     }
+            // }
+
+            // if文によってリンクを貼るかどうかを決定
+            if ($page_now < $page_max)                    // 現在のページ番号が最後のページ番号未満の場合
+            {
+                print '<a href=p_phisical_test_top.php?page_id=' . ($page_now + 1) . '>次へ</a>' . '　';
+            } else                                        // 現在のページ番号と最後のページ番号が等しい場合（現在のページ番号は最後のページ番より大きくならないため）
+            {
+                print '次へ';
+            }
+
+            print '<br><br>';
+            print '<input type="button" onclick="location.href=\'c_phisical_test_top.php\'" value="戻る">';
+            print '<input type="submit" name="delete" value="削除">';
+            print '<input type="submit" name="result" value="成績表">';
+        }
     } catch (Exception $e) {
         var_dump($e);
         exit();
