73e8272e85f102b3d2d42ffdac8422c8f4f5390b
tks007-github <takeshi.n.2020@gmail.com>
2021-08-27 06:03:11 +0900

    update

--- development/player/p_phisical_info/p_phisical_info_graph_height.php
+++ development/player/p_phisical_info/p_phisical_info_graph_height.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['p_login'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>p_phisical_info_graph</title>
+    <title>p_phisical_info_graph_height</title>
 </head>
 
 <body>
@@ -35,11 +35,6 @@ if (!isset($_SESSION['p_login'])) {
     <?php
     // player_codeをSESSIONで受け取る
     $player_code = $_SESSION['player_code'];
-    // graph_height、graph_weight、graph_body_fat、graph_muscle_massをSESSIONで受け取る
-    $graph_height = $_SESSION['graph_height'];
-    $graph_weight = $_SESSION['graph_weight'];
-    $graph_body_fat = $_SESSION['graph_body_fat'];
-    $graph_muscle_mass = $_SESSION['graph_muscle_mass'];
 
     try {
         date_default_timezone_set('Asia/Tokyo');
@@ -49,7 +44,7 @@ if (!isset($_SESSION['p_login'])) {
         $current_month = date('m');
         // 現在の年と月を結合(例. 2021年08月 → 202108)
         $year_month = $current_year . $current_month;
-        
+
         // db_academyデータベースに接続する
         $dsn = 'mysql:dbname=db_academy;host=localhost;charset=utf8';
         $user = 'root';
@@ -58,27 +53,65 @@ if (!isset($_SESSION['p_login'])) {
         $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
         // phisical_infoテーブルからplayer_codeを使って過去1年間の月平均のデータを検索
+        $year_month_arr = [];
+        $avg_height_arr = [];
 
-        $sql = '
+        // 今年分
+        for ($i = 0; $i < $current_month; $i++) {
+            $sql = '
                 SELECT DATE_FORMAT(date, \'%Y%m\') AS grouping_date, 
                 AVG(height) AS grouping_height
                 FROM phisical_info
                 WHERE player_code = ?
                 GROUP BY grouping_date
                 HAVING grouping_date = ?
-                ORDER BY grouping_date DESC
                 ';
-        $stmt = $dbh->prepare($sql);
-        $data[] = $player_code;
-        $data[] = $year_month - 1;
-        $stmt->execute($data);
-        $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+            $stmt = $dbh->prepare($sql);
+            $data[0] = $player_code;
+            $data[1] = $year_month - $i;
+            $stmt->execute($data);
+            $rec = $stmt->fetch(PDO::FETCH_ASSOC);
 
+            if ($rec == '') {
+                $year_month_arr[] = $current_year . '/' . ($current_month - $i);
+                $avg_height_arr[] = 0;
+            } else {
+                $year_month_arr[] = $current_year . '/' . ($current_month - $i);
+                $avg_height_arr[] = $rec['grouping_height'];
+            }
+        }
+
+        // 去年分
+        $year_month = ($current_year - 1) . 12;
+        for ($i = 0; $i < (12 - $current_month); $i++) {
+            $sql = '
+                SELECT DATE_FORMAT(date, \'%Y%m\') AS grouping_date, 
+                AVG(height) AS grouping_height
+                FROM phisical_info
+                WHERE player_code = ?
+                GROUP BY grouping_date
+                HAVING grouping_date = ?
+                ';
+            $stmt = $dbh->prepare($sql);
+            $data[0] = $player_code;
+            $data[1] = $year_month - $i;
+            $stmt->execute($data);
+            $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+            if ($rec == '') {
+                $year_month_arr[] = ($current_year - 1) . '/' . (12 - $i);
+                $avg_height_arr[] = 0;
+            } else {
+                $year_month_arr[] = ($current_year - 1) . '/' . (12 - $i);
+                $avg_height_arr[] = $rec['grouping_height'];
+            }
+        }
+
+        var_dump($year_month_arr);
+        var_dump($avg_height_arr);
 
         // db_academyデータベースから切断する
         $dbh = null;
-
-        var_dump($rec);
     } catch (Exception $e) {
         var_dump($e);
         exit();
--- development/player/p_phisical_info/p_phisical_info_top.php
+++ development/player/p_phisical_info/p_phisical_info_top.php
@@ -41,11 +41,7 @@
     $_SESSION['weight'] = '';                      
     $_SESSION['body_fat'] = '';                  
     $_SESSION['muscle_mass'] = '';
-    $_SESSION['graph_height'] = 1;
-    $_SESSION['graph_weight'] = 0;
-    $_SESSION['graph_body_fat'] = 0;
-    $_SESSION['graph_muscle_mass'] = 0;           
-
+    
     try {
         // player_codeをセッションで受け取る
         $player_code = $_SESSION['player_code'];
