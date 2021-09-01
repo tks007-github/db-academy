e462d1d8743bfba4406264baa6452af19d084c7b
tks007-github <takeshi.n.2020@gmail.com>
2021-09-01 16:58:03 +0900

    update

--- development/player/p_phisical_test/p_phisical_test_result.php
+++ development/player/p_phisical_test/p_phisical_test_result.php
@@ -25,7 +25,7 @@ if (!isset($_SESSION['p_login'])) {
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>p_phisical_test_result</title>
+    <title>p_phisical_test_result(総合)</title>
 </head>
 
 <body>
