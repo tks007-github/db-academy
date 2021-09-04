44368f545c71f04771969fc77709dce598b4dc1d
tks007-github <takeshi.n.2020@gmail.com>
2021-09-04 13:29:20 +0900

    update

--- new_development/player/p_top/p_top_password_change_done.php
+++ new_development/player/p_top/p_top_password_change_done.php
@@ -1,23 +1,24 @@
 <?php
-    session_start();
-    session_regenerate_id(true);
-    if (!isset($_SESSION['p_login'])) {
-        print 'ログインされていません。<br>';
-        print '<a href="p_top_login.html">ログイン画面へ</a>';
-        exit();
+session_start();
+session_regenerate_id(true);
+if (!isset($_SESSION['p_login'])) {     // 選手でログイン状態でない場合(SESSION['p_login']が未定義の場合)
+    print 'ログインされていません。<br>';
+    print '<a href="p_top_login.php">ログイン画面へ</a>';
+    exit();
+} else {                                // 選手でログイン状態の場合(SESSION['p_login']が定義されている(=1)の場合)
+    if (!isset($_SESSION['c_login'])) {         // 管理者でログイン状態の場合(SESSION[''])
+        print $_SESSION['player_name'];
+        print 'さんログイン中<br>';
+        print '<br>';
     } else {
-        if (!isset($_SESSION['c_login'])) {
-            print $_SESSION['player_name'];
-            print 'さんログイン中<br>';
-            print '<br>';
-        } else {
-            print $_SESSION['coach_name'];
-            print 'さんログイン中<br>';
-            print '選手検索：' . $_SESSION['player_name'];
-        }
-        
+        print $_SESSION['coach_name'];
+        print 'さんログイン中<br>';
+        print '選手検索：' . $_SESSION['player_name'];
     }
+}
+
 ?>
+
 
 <!DOCTYPE html>
 <html lang="ja">
@@ -26,7 +27,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>p_top_pass_change_done</title>
+    <title>p_top_password_change_done.php</title>
 </head>
 
 <body>
@@ -34,14 +35,16 @@
     <h3>パスワード変更完了</h3>
 
     <?php
+
+    // player_codeをセッションで受け取る
+    $player_code = $_SESSION['player_code'];
+
+    // p_top_password_change.phpから渡された値をセッションで受け取る
+    $new_player_password = $_SESSION['new_player_password'];
+    // new_player_passwordをmd5で暗号化
+    $new_player_password = md5($new_player_password);
+
     try {
-        // player_codeをセッションで受け取る
-        $player_code = $_SESSION['player_code'];
-
-        // p_top_pass_change.phpから渡された値をセッションで受け取る
-        $new_player_password = $_SESSION['new_player_password'];
-        $new_player_password = md5($new_player_password);
-
         // db_academyデータベースに接続
         $dsn = 'mysql:dbname=db_academy;host=localhost;charset=utf8mb4';
         $user = 'root';
@@ -58,14 +61,13 @@
 
         // player_managementデータベースから切断
         $dbh = null;
-
-        print 'パスワードを変更しました<br>';
-        print '新しいパスワード：' . $_SESSION['new_player_password'] . '<br>';
-        
     } catch (Exception $e) {
         var_dump($e);
         exit();
     }
+
+    print 'パスワードを変更しました<br>';
+    print '新しいパスワード：' . $_SESSION['new_player_password'] . '<br>';
 
     ?>
 
