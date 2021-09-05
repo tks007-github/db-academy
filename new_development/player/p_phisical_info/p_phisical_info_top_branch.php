6f689a22bc3fb583b17a00d48cad4c6e806c0c5d
tks007-github <takeshi.n.2020@gmail.com>
2021-09-05 14:41:42 +0900

    update

--- new_development/player/p_phisical_info/p_phisical_info_top_branch.php
+++ new_development/player/p_phisical_info/p_phisical_info_top_branch.php
@@ -1,19 +1,27 @@
+<!-- 
+    身体情報の入力データがあるかないかで遷移先を決定する。
+    入力データあり→p_questionnaire_add.php
+    入力データなし→p_questionnaire_edit.php
+ -->
+
 <?php
 session_start();
 session_regenerate_id(true);
 
+
+// p_phisical_infoディレクトリで使うSESSIONを初期化
+$_SESSION['phisical_info_flg'] = '';
+$_SESSION['date'] = '';
+$_SESSION['height'] = '';
+$_SESSION['weight'] = '';
+$_SESSION['body_fat'] = '';
+$_SESSION['muscle_mass'] = '';
+
+// SESSION変数からplayer_codeを受け取る
+$player_code = $_SESSION['player_code'];
+
+// DB接続
 try {
-    // p_phisical_infoディレクトリで使うSESSIONを初期化
-    $_SESSION['phisical_info_flg'] = '';
-    $_SESSION['date'] = '';
-    $_SESSION['height'] = '';
-    $_SESSION['weight'] = '';
-    $_SESSION['body_fat'] = '';
-    $_SESSION['muscle_mass'] = '';
-
-    // player_codeをセッションで受け取る
-    $player_code = $_SESSION['player_code'];
-
     // db_academyデータベースに接続する
     $dsn = 'mysql:dbname=db_academy;host=localhost;charset=utf8';
     $user = 'root';
@@ -21,7 +29,7 @@ try {
     $dbh = new PDO($dsn, $user, $password);
     $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-    // phisical_infoテーブルからplayer_codeを使って情報を検索
+    // phisical_infoテーブルから選手コード(player_code)を使って最新の情報を検索
     $sql = '
             SELECT date, height, weight, body_fat, muscle_mass 
             FROM phisical_info 
@@ -40,15 +48,15 @@ try {
 
     // db_academyデータベースから切断する
     $dbh = null;
-
-    if ($rec == '') {                     // データベースからの問い合わせ結果がない場合
-        header('Location: p_phisical_info_first_add.php');
-        exit();
-    } else {                              // データベースからの問い合わせ結果があった場合
-        header('Location: p_phisical_info_top.php');
-        exit();
-    }
 } catch (Exception $e) {
     var_dump($e);
+    exit();
+}
+
+if ($rec == '') {                     // データベースからの問い合わせ結果がない場合
+    header('Location: p_phisical_info_first_add.php');
+    exit();
+} else {                              // データベースからの問い合わせ結果があった場合
+    header('Location: p_phisical_info_top.php');
     exit();
 }
