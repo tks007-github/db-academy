067c8732cd55e118f352a2abce596369b2b8545d
tks007-github <takeshi.n.2020@gmail.com>
2021-09-08 08:49:27 +0900

    update

--- new_development/player/p_phisical_info/p_phisical_info_edit_check.php
+++ new_development/player/p_phisical_info/p_phisical_info_edit_check.php
@@ -1,6 +1,14 @@
+<!-- 
+    p_phisical_info_edit.phpからの入力情報をチェックし、遷移先を決定する。
+    問題なし→p_phisical_info_edit_done.phpへリダイレクト
+    問題あり→エラーメッセージを出力し、戻るボタンでp_phisical_info_edit.phpへ遷移
+ -->
+
 <?php
 session_start();
 session_regenerate_id(true);
+
+// SESSION変数からplayer_codeを受け取る
 $player_code = $_SESSION['player_code'];
 
 // 自作の関数を呼び出す
@@ -15,32 +23,70 @@ $weight = $post['weight'];
 $body_fat = $post['body_fat'];
 $muscle_mass = $post['muscle_mass'];
 
+// DB接続
+try {
+    // db_academyデータベースに接続する
+    $dsn = 'mysql:dbname=db_academy;host=localhost;charset=utf8';
+    $user = 'root';
+    $password = 'root';
+    $dbh = new PDO($dsn, $user, $password);
+    $dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    // phisical_infoテーブルからplayer_codeとdateを使って情報を検索
+    $sql = '
+            SELECT * 
+            FROM phisical_info 
+            WHERE player_code = ? 
+            AND date = ?
+            ';
+    $stmt = $dbh->prepare($sql);
+    $data[] = $player_code;
+    $data[] = $date;
+    $stmt->execute($data);
+    $rec = $stmt->fetch(PDO::FETCH_ASSOC);
+
+    // db_academyデータベースから切断する
+    $dbh = null;
+} catch (Exception $e) {
+    var_dump($e);
+    exit();
+}
 
 // 入力内容の問題の有無をflgによって判定(問題なし:true、問題あり:false)
 $flg = true;
 
-if ($date == '') {
+// 日付について
+if ($date == '') {                              // 日付が入力されていない場合
+    print '日付が入力されていません<br>';
+    $flg = false;
+} else if ($rec2 != '') {                       // 入力された日付のデータがすでに存在する場合
+    print '同じ日付のデータがすでに存在します<br>';
     $flg = false;
 }
 
+// 身長について
 if ($height == '') {
+    print '身長が入力されていません<br>';
     $flg = false;
 }
 
+// 体重について
 if ($weight == '') {
+    print '体重が入力されていません<br>';
     $flg = false;
 }
 
+// 体脂肪率について
 if ($body_fat == '') {
+    print '体脂肪率が入力されていません<br>';
     $flg = false;
 }
 
+// 筋量について
 if ($muscle_mass == '') {
+    print '筋量が入力されていません<br>';
     $flg = false;
 }
-
-// db_academyデータベースから切断する
-$dbh = null;
 
 if ($flg) {             // 入力に問題がなかった場合
     $_SESSION['date'] = $date;                          // セッション変数にdateを保持
