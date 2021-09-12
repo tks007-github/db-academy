cda265c6ebb5cb9cd9cad1c05732cdf642a2b070
tks007-github <takeshi.n.2020@gmail.com>
2021-09-12 15:12:13 +0900

    update

--- new_development/coach/c_phisical_test/c_phisical_test_delete_check.php
+++ new_development/coach/c_phisical_test/c_phisical_test_delete_check.php
@@ -49,17 +49,6 @@ if ($rec == '') {       // 選手の記録が登録されていない場合
     header('Location:c_phisical_test_delete_done.php');    // c_phisical_test_delete_done.phpへリダイレクト
     exit();
 } else {                // すでに選手の記録が登録されている場合
-    
-    if (!isset($_SESSION['c_login'])) {     // コーチでログイン状態でない場合(SESSION['c_login']が未定義の場合)
-        print 'ログインされていません。<br>';
-        print '<a href="../c_top/c_top_login.php">ログイン画面へ</a>';
-        exit();
-    } else {                                // コーチでログイン状態の場合(SESSION['c_login']が定義されている(=1)場合)
-        print $_SESSION['coach_name'];
-        print 'さんログイン中<br>';
-        print '<br>';
-    }
-
     print '選手の記録が登録されているので削除できません<br><br>';
     print '<input type="button" onclick="location.href=\'c_phisical_test_content.php\'" value="戻る">';
 }
